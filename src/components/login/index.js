f00bc23e9bc2de3274217c1fb2938f6eed02dbf5
karenmoreno17 <53530934+karenmoreno17@users.noreply.github.com>
2022-08-13 20:24:19 -0500

    Update index.js
    Signed-off-by: karenmoreno17 <53530934+karenmoreno17@users.noreply.github.com>

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -4,6 +4,17 @@ import styles from '/src/styles/Home.module.css'
 
 export default function Inicio() {
   return <div className={styles.grid}>
+
+    <h1 className={styles.title}>
+      Bienvenido a <span className={styles.highlight}>mande!</span>
+    </h1>
+
+    <p className={styles.description}>
+      Consigue personas trabajadoras, expertas y honestas para realizar a domicilio
+      labores del día a día en el hogar. Si lo deseas también puedes ser parte de
+      nuestro equipo de trabajo.
+    </p>
+
     <nav>
       <Link href="/userRegister">
         <a>
